42aa5dd1f7a35d685ec1c036bfef92a2b756e556
Lsong <song940@gmail.com>
2018-08-31 13:24:28 +0800

    modify ofo

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
+const url = require('url');
 const http = require('http');
 const https = require('https');
 const qs = require('querystring');
 
-const fetch = (path, data) => new Promise((resolve, reject) => {
-  const req = https.request({
-    method: 'post',
-    hostname: 'one.ofo.com',
-    path,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  }, res => {
-    let buffer = '';
-    res
-      .on('error', reject)
-      .on('data', chunk => buffer += chunk)
-      .on('end', () => {
-        const { errorCode, msg, values } = JSON.parse(buffer);
-        if(errorCode === 200){
-          resolve(values)
-        }else{
-          const error = new Error(msg);
-          error.code = errorCode;
-          error.values = values;
-          reject(error);
-        }
-      });
-  });
-  req.write(qs.stringify(data));
-  req.end();
-});
-
 const ofo =  {
+  api: 'http://one.ofo.com',
+  post(path, form){
+    const { hostname } = url.parse(this.api);
+    return new Promise((resolve, reject) => {
+      const req = https.request({
+        method: 'post',
+        hostname,
+        path,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }, (res, buffer = '') => {
+        res
+        .on('error', reject)
+        .on('data', chunk => buffer += chunk)
+        .on('end', () => {
+          const { errorCode, msg, values } = JSON.parse(buffer);
+          if(errorCode === 200){
+            resolve(values)
+          }else{
+            const error = new Error(msg);
+            error.code = errorCode;
+            error.values = values;
+            reject(error);
+          }
+        });
+      });
+      req.on('error', reject);
+      req.write(qs.stringify(form));
+      req.end();
+    });
+  },
   code(tel, { lat, lng, ccc = '86' } = {}){
-    return fetch('/verifyCode_v2', { tel, type: 1, lat, lng, ccc });
+    return this.post('/verifyCode_v2', { tel, type: 1, lat, lng, ccc });
   },
   login(tel, code, { lat, lng, ccc = '86' } = {}){
-    return fetch('/api/login_v2', { tel, code, ccc, lat, lng });
+    return this.post('/api/login_v2', { tel, code, ccc, lat, lng });
   },
   near(token, { lat, lng, source = 2 } = {}){
-    return fetch('/nearbyofoCar', { lat, lng, token, source });
+    return this.post('/nearbyofoCar', { lat, lng, token, source });
   }
 }
 
